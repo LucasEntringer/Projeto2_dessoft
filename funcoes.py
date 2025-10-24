0a29752d94ae1a804ba7666cc6fb183f3f862d50
Lucas <lwae2508@gmail.com>
2025-10-24 00:48:28 -0300

    Exercicio.tentativa_2

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,11 @@
-def define_posicoes (linha, coluna, orientação, tamanho):
+def define_posicoes (linha, coluna, orientacao, tamanho):
     res = []
-    if orientação == 'vertical':
+    if orientacao == 'vertical':
         for i in range(tamanho):
             casa = [linha + i, coluna]
-            res.append(casa)
+            res.append([casa])
     else:
         for i in range(tamanho):
             casa = [linha, coluna + i]
-            res.append(casa)
+            res.append([casa])
     return res
