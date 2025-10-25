227fd8213b05c3577e0223784278b2b62d8f6000
Lucas <lwae2508@gmail.com>
2025-10-24 22:50:43 -0300

    talvez foi

--- programa.py
+++ programa.py
@@ -22,15 +22,26 @@ quantidade = {
 }
 
 for navio, valor in tamanho.items():
-    for i in range(quantidade[navio]):
-        print(f'Insira as informações referentes ao navio {navio} que possui tamanho {valor}')
-        linha = int(input('Linha: '))
-        coluna = int(input('Coluna: '))
-        orientacao = 'vertical'
-        if navio != 'submarino':
-            direcao = int(input('Digite 1 para vertical ou 2 para horizontal: '))
-            if direcao == 2:
-                orientacao = 'horizontal'
-        if posicao_valida(frota, linha, coluna, orientacao, tamanho) == True:
-            frota = preenche_frota(frota, navio, linha, coluna, orientacao, tamanho)
-        else: print('Esta posição não é válida')
+    # for i in range(quantidade[navio]):
+    i = 1
+    while i <= quantidade[navio]:
+        j = 0
+        while j == 0:
+            print(f'Insira as informações referentes ao navio {navio} que possui tamanho {valor}')
+            linha = int(input('Linha: '))
+            coluna = int(input('Coluna: '))
+            orientacao = 'vertical'
+            tam = tamanho[navio]
+            if navio != 'submarino':
+                direcao = int(input('Digite 1 para vertical ou 2 para horizontal: '))
+                if direcao == 2:
+                    orientacao = 'horizontal'
+                elif direcao != 2 and direcao != 1:
+                    print('Esta posição não é válida')
+                    break
+            if posicao_valida(frota, linha, coluna, orientacao, tam) == True:
+                frota = preenche_frota(frota, navio, linha, coluna, orientacao, tam)
+                j = 1
+                break
+            else: print('Esta posição não é válida')
+print(frota)
