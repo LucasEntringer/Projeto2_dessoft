6d8cd11fbcad9c502dd17dfd0ad5264db2df2a0d
Lucas <lwae2508@gmail.com>
2025-10-24 23:47:21 -0300

    não da pra deixar mais rápido

--- programa.py
+++ programa.py
@@ -1,5 +1,6 @@
 from funcoes import define_posicoes, preenche_frota, faz_jogada, posiciona_frota, afundados, posicao_valida, monta_tabuleiros
 import random
+random.seed(2)
 
 frota = {
     "porta-aviões":[],
